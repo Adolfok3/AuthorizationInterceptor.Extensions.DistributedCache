8e2f846897c2cdd30a089117610ceb60e5dd3f2b
Luiz Adolfo <luiz-adolfo@hotmail.com>
2024-11-19 09:15:38 -0300

    Implemented package

---
+++ src/Extensions/AuthorizationInterceptorOptionsExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using AuthorizationInterceptor.Extensions.Abstractions.Options;
+using AuthorizationInterceptor.Extensions.DistributedCache.Interceptors;
+
+namespace AuthorizationInterceptor.Extensions.DistributedCache.Extensions
+{
+    /// <summary>
+    /// Extension methods that Configures the authorization interceptor to use a distributed cache interceptor for <see cref="IAuthorizationInterceptorOptions"/>
+    /// </summary>
+    public static class AuthorizationInterceptorOptionsExtensions
+    {
+        /// <summary>
+        /// Configures the authorization interceptor to use a distributed cache interceptor.
+        /// </summary>
+        /// <param name="options"><see cref="IAuthorizationInterceptorOptions"/></param>
+        /// <returns><see cref="IAuthorizationInterceptorOptions"/></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static IAuthorizationInterceptorOptions UseDistributedCacheInterceptor(this IAuthorizationInterceptorOptions options)
+        {
+            options.UseCustomInterceptor<DistributedCacheAuthorizationInterceptor>();
+            return options;
+        }
+    }
+}
